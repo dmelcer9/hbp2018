546332b13d44501ad0fb89df6404bdbbfb3b30ab
dmelcer9 <dmelcer9@gmail.com>
2018-02-10 00:55:20 -0500

    Verification works I guess

--- index.js
+++ index.js
@@ -1,3 +1,69 @@
 const db =  require("./models/index.js");
 
-console.log(db.User);
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
+
+var models = require('./models');
+
+var bcrypt = require('bcrypt');
+
+//Returns a user instance or null
+async function getUserAuth(uname, pass){
+  var user = await db.User.findOne({where:{uname:uname}});
+  if(!user){
+    return false;
+  }
+  var valid = await bcrypt.compare(pass,user.passwordHashed);
+  if(valid) return user;
+  return false;
+}
+
+app.get('/verifyLogin', async function(req, res){
+  var body = req.headers;
+  console.log(body.uname);
+  if(!body.uname || !body.password){
+    res.status(500).send("Invalid params");
+    return
+  }
+
+  var user = await getUserAuth(body.uname, body.password);
+  if(user){
+    res.send("Valid");
+  } else{
+    res.send("Invalid");
+  }
+})
+
+
+app.post('/createUser', async function(req, res){
+  var body = req.body;
+
+  if(!body.firstName || !body.lastName || !body.uname || !body.password){
+    res.status(500).send("Invalid params");
+    return;
+  }
+  var hashed = await bcrypt.hash(body.password, 10);
+  console.log(hashed);
+  await models.User.create({
+    firstName:body.firstName,
+    lastName:body.lastName,
+    uname:body.uname,
+    passwordHashed: hashed
+  });
+
+  res.status(200).send("Created User");
+})
+
+var f = async function(){
+  await db.User.sync();
+
+  console.log("Listening");
+  app.listen(8080);
+
+};
+
+f();
--- models/user.js
+++ models/user.js
@@ -1,13 +1,12 @@
 'use strict';
 
 module.exports = (sequelize, DataTypes) => {
-  var Person = sequelize.define('User', {
+  var User = sequelize.define('User', {
     firstName: {type: DataTypes.STRING,allowNull: false},
     lastName: {type: DataTypes.STRING,allowNull: false},
     uname: {type: DataTypes.STRING,allowNull: false},
     passwordHashed: {type: DataTypes.STRING,allowNull: false}
-  }
-  })
+  });
 
-  return Person;
+  return User;
 }
